120fb6ba0a3de00425d9290f189c99959ab29fe8
Wilson Chen <wilson.chen@codeimmersives.com>
2020-04-13 16:20:54 -0400

    finish

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 const Stack = function() {
+  return {
+    items: [],
+    add: function(name){
+      this.items.push(name)
+    },
+    remove: function(){
+      const toBe = this.items[this.items.length-1]
+      this.items.pop()
+      return toBe
+    },
+    peek:function(){
+      return this.items[this.items.length-1]
+    }
+  }
 }
 
 const Queue = function() {
+  return {
+    items: [],
+    add: function(name){
+      this.items.push(name)
+    },
+    remove: function(){
+      const toBe = this.items[0]
+      this.items.shift()
+      return toBe
+    },
+    peek: function(){
+      return this.items[0]
+    }
+  }
 }
 
 module.exports = {
